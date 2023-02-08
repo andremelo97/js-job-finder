4d8cbbd8ccc6c68002a4312bd2a37edae70e7777
André Melo <andre.melo9715@gmail.com>
2023-02-08 14:11:50 -0300

    primeiro commit

---
+++ db/connection.js
@@ -0,0 +1,8 @@
+// 03. Criando e exportando o DB
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './db/app.db'
+});
+
+module.exports = sequelize;
